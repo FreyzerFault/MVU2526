62fe7ba924bc420200d1e0f3e6296f629ec530d9
David Martinez Alcantara <martinezalcantara29@gmail.com>
2026-05-07 18:23:46 +0200

    Cambio de nombre del Proyecto de Consola de Comandos + Variables de entorno

---
+++ tools/MVU2526_tools/MVU2526_tools/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Inicio de la Aplicación de Consola");
+
+// ===================================================================================================================
+// Invocar a Unity desde el CMD
+
+// COMANDO para CMD:
+// "C:\Program Files\Unity\Hub\Editor\6000.3.11f1\Editor\unity.exe" -batchmode -projectPath "C:\Unity\MVU2526\MVU2526 - Fork" -buildWindows64Player "C:\Unity\MVU2526\MVU2526 - Fork\Build\Windows\Game.exe" -quit
+
+string unityEditorPath = Environment.GetEnvironmentVariable("MVU2526_UNITY") ?? @"C:\Program Files\Unity\Hub\Editor\6000.3.11f1\Editor\unity.exe";
+string projectPath = Environment.GetEnvironmentVariable("MVU2526_PROJECT") ?? @"C:\Unity\MVU2526";
+string unityProjectPath = Path.Combine(projectPath, @"\MVU2526 - Fork");
+string buildPath = Path.Combine(projectPath, @"\MVU2526 - Fork\Build\Windows\Game.exe");
+
+const bool batchMode = true;
+
+string arguments = $"{(batchMode ? "-batchmode" : "")} -projectPath \"{unityProjectPath}\" -buildWindows64Player \"{buildPath}\" -quit";
+
+Console.WriteLine($"Ejecutando comando\n´{unityEditorPath} {arguments}");
+
+Process buildProcess = Process.Start(unityEditorPath, arguments);
+
+buildProcess.WaitForExit();
+
+// ===================================================================================================================
